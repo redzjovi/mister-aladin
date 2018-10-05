33727bfb4deb18ec4fe21091b20bda098fc50a32
Jovi <redzjovi@gmail.com>
2018-10-05 11:44:36 +0700

    [User] add api/users.
    - GET api/users
    - POST api/users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('phone_number');
+
+            $table->text('image')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
